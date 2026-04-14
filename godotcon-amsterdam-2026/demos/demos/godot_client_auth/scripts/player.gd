9714f5bff6ac5c017390ecdfb0dc24bcdf5c3ac5
Tamás Gálffy <ezittgtx@gmail.com>
2026-04-14 16:07:25 +0200

    some refactor

---
+++ godotcon-amsterdam-2026/demos/demos/godot_client_auth/scripts/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody3D
+
+@export var move_speed := 8.
+@export var gravity := 24.
+
+func _ready() -> void:
+	# Set player color
+	var color := Utils.generate_player_color(get_multiplayer_authority())
+	Utils.override_mesh_color(self, color)
+
+func _physics_process(dt: float) -> void:
+	if not is_multiplayer_authority():
+		return
+
+	var input_movement := Vector3(
+		Input.get_axis("move_west", "move_east"),
+		0.,
+		Input.get_axis("move_north", "move_south")
+	)
+	if not DisplayServer.window_is_focused():
+		input_movement = Vector3.ZERO
+
+	var movement_basis := get_viewport().get_camera_3d().basis
+
+	var movement := movement_basis * input_movement
+	movement.y = 0.
+	movement = movement.normalized() * move_speed
+
+	velocity.x = movement.x
+	velocity.y -= gravity * dt
+	velocity.z = movement.z
+
+	move_and_slide()
---
+++ godotcon-amsterdam-2026/demos/shared/scripts/spawner.gd
@@ -0,0 +1,36 @@
+extends MultiplayerSpawner
+
+@export var spawn_points: Array[Node3D]
+
+var _avatars := {} as Dictionary[int, Node3D]
+
+func _ready() -> void:
+	spawn_function = _spawn
+
+	NetworkEvents.on_server_start.connect(func(): spawn(1))
+	NetworkEvents.on_peer_join.connect(func(peer: int): spawn(peer))
+	NetworkEvents.on_peer_leave.connect(func(peer: int): _despawn(peer))
+
+func _spawn(peer: int) -> Node3D:
+	var spawnable_scene := load(get_spawnable_scene(0)) as PackedScene
+	var avatar := spawnable_scene.instantiate() as Node3D
+
+	if avatar.has_method("set_peer"):
+		avatar.set_peer(peer)
+	else:
+		avatar.set_multiplayer_authority(peer)
+	_avatars[peer] = avatar
+
+	var spawn_idx := peer % spawn_points.size()
+	avatar.position = spawn_points[spawn_idx].global_position
+	avatar.name += " #%d" % peer
+
+	return avatar
+
+func _despawn(peer: int) -> void:
+	if not _avatars.has(peer):
+		return
+
+	var avatar := _avatars[peer]
+	avatar.queue_free()
+	_avatars.erase(peer)
